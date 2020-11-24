495c1454e2f3ec3fd5ab2ae235469eae4dc5cf6a
JosueDLA <josuedlavs@gmail.com>
2020-11-24 00:13:33 -0600

    Update dockstrings of triangle module.

--- shape_detection/triangle.py
+++ shape_detection/triangle.py
@@ -3,7 +3,23 @@ import math
 
 
 class Triangle:
+    """Three-sided Poligon.
+        /C\       \r
+       /   \      \r
+     b/     \\a   \r
+     /       \    \r
+    A__________B  \r
+         c
+    """
+
     def __init__(self, point_a: Point, point_b: Point, point_c: Point):
+        """Triangle formed by 3 given points.
+
+        Args: 
+            point_a (Point): Point A
+            point_b (Point): Point B
+            point_c (Point): Point C
+        """
         self.point_a = point_a
         self.point_b = point_b
         self.point_c = point_c
@@ -12,41 +28,49 @@ class Triangle:
         return (f"{self.point_a} {self.point_b} {self.point_c}")
 
     def get_angle_a(self):
+        """Angle A° (alpha)."""
         return get_angle(self.point_a, self.point_b, self.point_c)
 
     def get_angle_b(self):
+        """Angle B° (beta)."""
         return get_angle(self.point_b, self.point_c, self.point_a)
 
     def get_angle_c(self):
+        """Angle C° (gama)."""
         return get_angle(self.point_c, self.point_a, self.point_b)
 
     def get_side_a(self):
+        """Line a length"""
         return get_distance(self.point_b, self.point_c)
 
     def get_side_b(self):
+        """Line b length"""
         return get_distance(self.point_a, self.point_c)
 
     def get_side_c(self):
+        """Line c length"""
         return get_distance(self.point_a, self.point_b)
 
 
 def get_angle(point_a: Point, point_b: Point, point_c: Point):
-    """
-    Get the angle between three points
-    Returns the angle at Aº formed by the triangle ABC
+    """Get the angle between three points.
 
-    cos(Aº) = (b^2 + c^2 - a^2) / 2bc
-    Aº = cos^-1((b^2 + c^2 - a^2) / 2bc)
+    cos(A°) = (b^2 + c^2 - a^2) / 2bc
 
-        /C\
-       /   \
-     b/     \a
-     /       \
-    A_________B
-         c
+    A° = cos^-1((b^2 + c^2 - a^2) / 2bc)
 
-    Source: https://www.mathsisfun.com/algebra/trig-solving-sss-triangles.html
-            https://www.mathsisfun.com/algebra/trig-cosine-law.html
+    Args:
+        point_a (Point): Point A
+        point_b (Point): Point B
+        point_c (Point): Point C
+
+    Returns:
+        float: angle at A° (alpha) formed by the triangle ABC
+
+    Notes: 
+        For more information go to:
+        https://www.mathsisfun.com/algebra/trig-solving-sss-triangles.html
+        https://www.mathsisfun.com/algebra/trig-cosine-law.html
     """
     a = get_distance(point_b, point_c)
     b = get_distance(point_a, point_c)
